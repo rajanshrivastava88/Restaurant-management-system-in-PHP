b5aeffe73430f0032df7d5cd7aa63867af55420b
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2025-03-20 16:06:12 +0530

    Update loginck.php

--- loginck.php
+++ loginck.php
@@ -4,18 +4,12 @@
 $u = $_POST['uid'];
 $p = $_POST['pass'];
 include  "connect.php";
-$s = mysqli_query($con,"select * from registration where userid='$u' and password='$p'");
+$s = mysqli_query($con, "select * from registration where userid='$u' and password='$p'");
 
-if($r = mysqli_fetch_array($s))
-{
-		$_SESSION['uid'] = $u;
-		header("location:index.php");
-
+if ($r = mysqli_fetch_array($s)) {
+	$_SESSION['uid'] = $u;
+	header("location:index.php");
+} else {
+	echo "<br><div style='color:black; border-radius:10px; padding:10px; text-align:center; background-color:tomato;'>Please Enter Valid User and password</div><br>";
+	include "login.php";
 }
-else
-{
-		echo "<br><div style='color:black; border-radius:10px; padding:10px; text-align:center; background-color:tomato;'>Please Enter Valid User and password</div><br>";
-		include "login.php";
-}
-
-?>
